aeb5206b8ac7d006396de0beb3f4f458d9f6e894
Eduardo Cabrera <lalo@eof.cl>
2022-03-15 13:53:03 -0300

    Se agrega CrossOrigin *

--- src/main/java/com/devops/dxc/devops/rest/RestData.java
+++ src/main/java/com/devops/dxc/devops/rest/RestData.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/rest/msdxc")
 public class RestData {
 	
